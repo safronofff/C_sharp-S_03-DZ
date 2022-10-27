97b048d69cbdfd5075e6ae36674f83556d1557af
ys-off <yuriy.safronoff@yandex.ru>
2022-10-27 19:47:53 +0300

    03

---
+++ 03/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+for (int y = 1; y <= x; y++)
+{
+	int z = y * y * y;
+	Console.WriteLine(z);
+}
